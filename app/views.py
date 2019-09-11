185d1ca34eb9daf0260b6ed8694cdb6221d4b82a
SoriveZJQ <41794547+SoriveZJQ@users.noreply.github.com>
2019-09-11 16:07:47 +0800

    test

---
+++ app/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, request
+from app.index import InfoForm
+from app.query import start
+from microblog import app
+import os
+import time
+
+
+UPLOAD_FOLDER='static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = InfoForm()
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    if request.method == 'POST' and form.validate_on_submit():
+        f = request.files['fileopen']
+        fname = f.filename
+        print(fname)
+        ext = fname.rsplit('.')[1]
+        unix_time = int(time.time())
+        new_filename = str(unix_time)+'.'+ext
+        last_filename = os.path.join(file_dir, new_filename)
+        f.save(last_filename)
+        info = start(last_filename, form.term.data, file_dir)
+
+        return render_template("query.html", form=form, flag=1, info=info)
+    return render_template('query.html', form=form, flag=0)
+
